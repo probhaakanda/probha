8d71fee43b2f2c834ea9c87f4b2f2f5e0161343b
probhaakanda <probhaakanda@gmail.com>
2024-08-26 10:14:53 +0600

    Set up NextAuth in `route.ts` - Configured GitHub, Google, and Twitter providers for authentication; fun fact: The first API call made on the web was from Tim Berners-Lee's original browser, which allowed users to access and retrieve documents from the World Wide Web in 1991.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,41 @@
+// app/api/auth/[...nextauth]/route.ts
+
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import TwitterProvider from "next-auth/providers/twitter";
+
+const authOptions = NextAuth({
+  theme:{
+    colorScheme:'auto'
+  },
+  providers: [
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+        ? process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+        : "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+        ? process.env.GITHUB_CLIENT_SECRET
+        : "",
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+        ? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+        : "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        ? process.env.GOOGLE_CLIENT_SECRET
+        : "",
+    }),
+    TwitterProvider({
+      clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID
+        ? process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID
+        : "",
+      clientSecret: process.env.TWITTER_CLIENT_SECRET
+        ? process.env.TWITTER_CLIENT_SECRET
+        : "",
+    })
+  ],
+  // Other NextAuth options can be added here
+});
+
+export { authOptions as POST, authOptions as GET };
